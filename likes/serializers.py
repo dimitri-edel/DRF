2da6df9074338205fc317d0da3cad0099cf57fe0
Dimitri Edel <dimitri.edel@gmx.net>
2023-07-10 18:27:55 +0200

    Add likes.views and urls

--- likes/serializers.py
+++ likes/serializers.py
@@ -16,9 +16,12 @@ class LikesSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
+    # Prevent duplicate likes
         try:
             return super().create(validated_data)
         except IntegrityError:
+        # Due to models.Likes.unique_together['post', 'owner]
+        # if the user attmpts to add a like twice an IntegrityError will be rasied        
             raise serializers.ValidationError({
                 'detail': 'Cannot like the same post twice!'
             })
--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from DRF_prj.permissions import IsOwnerOrReadOnly
+from likes.models import Likes
+from likes.serializers import LikesSerializer
 
-# Create your views here.
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
